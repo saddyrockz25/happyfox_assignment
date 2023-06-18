91e7d2a66a2f2eeeb393a3b81f9392d515fb3dc9
Sadan Sohan Madhavan <sadan@newlaptop.local>
2023-06-18 17:10:15 +0530

    More modularity in modify_emails.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from add_email_rules import add_email_rules
 if __name__ == '__main__':
     gmail_creds = authenticate()
     if gmail_creds:
-        # fetch_emails(gmail_creds)
-        # add_email_rules()
+        fetch_emails(gmail_creds)
+        add_email_rules()
         modify_emails(gmail_creds)
--- modify_emails.py
+++ modify_emails.py
@@ -9,15 +9,92 @@ import pandas as pd
 db_config_file = open('config.json')
 db_config = json.load(db_config_file)
 
+
+# Function to determine the labels that need to be added/removed to the filtered messages
+def prepare_labels_for_filtering_emails(move_message_label_id, message_read_label_id):
+
+    # all possible labels for emails
+    all_label_ids = ["INBOX", "SPAM", "TRASH", "UNREAD"]
+
+    # list of label_ids to associated with the filtered messages
+    add_label_ids = []
+
+    # initialize labels to be removed with all label ids
+    remove_label_ids = all_label_ids.copy()
+
+    # capture the action from the rule and add it to add_label_ids, remove it from remove_label_ids
+    # the filtered messages will be associated with add_label_ids and will be disassociated from the
+    # rest of the label_ids represented by remove_label_ids
+    add_label_ids.append(move_message_label_id)
+    remove_label_ids.remove(move_message_label_id)
+
+    # if the message needs to marked as unread, add it to add_label_ids and remove it from remove_label_ids
+    if message_read_label_id == "UNREAD":
+        remove_label_ids.remove(message_read_label_id)
+        add_label_ids.append(message_read_label_id)
+
+    return add_label_ids, remove_label_ids
+
+# Function to combine results of all filters for a rule depending on global predicate(all/any)
+def combine_all_filters(emails_df, query_results, global_predicate):
+
+    if global_predicate == "all":
+        final_result = pd.Series([True] * len(emails_df))
+        for each_query_result in query_results:
+            final_result = final_result & each_query_result
+    else:
+        final_result = pd.Series([False] * len(emails_df))
+        for each_query_result in query_results:
+            final_result = final_result | each_query_result
+
+    return emails_df[final_result]
+
+# Function to process rules that involve any string field
+def process_string_field_filter(emails_df, rule_key, rule_value, rule_predicate):
+    # map of key specified in the json rule to the column name in the database
+    rule_key_attribute_map = {
+        "from": "sender_email",
+        "subject": "subject",
+        "date_received": "date",
+        "message": "message_content"
+    }
+
+    if rule_predicate == "contains":
+        return (emails_df[rule_key_attribute_map[rule_key]].str.contains(rule_value))
+    elif rule_predicate == "does_not_contain":
+        return (~emails_df[rule_key_attribute_map[rule_key]].str.contains(rule_value))
+    elif rule_predicate == "equals":
+        return (emails_df[rule_key_attribute_map[rule_key]] == rule_value)
+    else:
+        return (~emails_df[rule_key_attribute_map[rule_key]] == rule_value)
+
+
+# Function to process rules that involve the date received field
+def process_date_field_filter(emails_df, rule_time_value, rule_time_unit, rule_predicate):
+
+    time_unit_map = {
+        "days": 1,
+        "months": 30
+    }
+
+    # compute the requisite start and end dates
+    today = datetime.now()
+    end_date = str(today.date())
+    start_date = str(datetime.fromtimestamp(math.floor(today.timestamp()) - \
+                                            (int(rule_time_value) * time_unit_map[rule_time_unit] * 86400)).date())
+
+    # build the query depending on the predicate
+    if rule_predicate == "less_than":
+        return (emails_df["date"].dt.strftime("%Y-%m-%d %H:%M:%S") > start_date) & \
+                             (emails_df["date"].dt.strftime("%Y-%m-%d %H:%M:%S") < end_date)
+    else:
+        return (emails_df["date"].dt.strftime("%Y-%m-%d %H:%M:%S") < start_date)
+
 # Function to modify emails in gmail based on rules in the json file
 def modify_emails(gmail_creds):
     email_rules_file = open('email_rules.json')
     rules = json.load(email_rules_file)
 
-    time_unit_map = {
-        "days":1,
-        "months":30
-    }
 
     # instantiate postgres connection
     postgres_engine = create_engine("postgresql://" + db_config["db_username"] + ":" + \
@@ -31,6 +108,9 @@ def modify_emails(gmail_creds):
         emails_df = pd.read_sql("select * from emails", connection)
 
         for rule_name in rules:
+
+            # Store the series of boolean array, that results from each query/filter in the rule
+            query_results = []
 
             # capture the global predicate for the rule - all/any
             global_predicate = list(rules[rule_name].keys())[0]
@@ -47,54 +127,15 @@ def modify_emails(gmail_creds):
                     rule_time_value = rule_value.split()[0]
                     rule_time_unit = rule_value.split()[1]
 
-                    # compute the requisite start and end dates
-                    today = datetime.now()
-                    end_date = str(today.date())
-                    start_date = str(datetime.fromtimestamp(math.floor(today.timestamp()) - \
-                    (int(rule_time_value) * time_unit_map[rule_time_unit] * 86400)).date())
-
-                    # build the query depending on the predicate
-                    if rule_predicate == "less_than":
-                        date_query = (emails_df["date"].dt.strftime("%Y-%m-%d %H:%M:%S") > start_date) & \
-                                     (emails_df["date"].dt.strftime("%Y-%m-%d %H:%M:%S") < end_date)
-                    else:
-                        date_query = (emails_df["date"].dt.strftime("%Y-%m-%d %H:%M:%S") < start_date)
+                    query_results.append(process_date_field_filter\
+                                             (emails_df, rule_time_value, rule_time_unit, rule_predicate))
                 else:
                     # similarly build queries for sender_email, subject and message content depending on predicate
-                    if rule_predicate == "contains":
-                        if rule_key == "from":
-                            sender_email_query = emails_df["sender_email"].str.contains(rule_value)
-                        elif rule_key == "subject":
-                            subject_query = emails_df["subject"].str.contains(rule_value)
-                        else:
-                            message_query = emails_df["message_content"].str.contains(rule_value)
-                    elif rule_predicate == "does_not_contain":
-                        if rule_key == "from":
-                            sender_email_query = ~emails_df["sender_email"].str.contains(rule_value)
-                        elif rule_key == "subject":
-                            subject_query = ~emails_df["subject"].str.contains(rule_value)
-                        else:
-                            message_query = ~emails_df["message_content"].str.contains(rule_value)
-                    elif rule_predicate == "equals":
-                        if rule_key == "from":
-                            sender_email_query = emails_df["sender_email"] == rule_value
-                        elif rule_key == "subject":
-                            subject_query = emails_df["subject"] == rule_value
-                        else:
-                            message_query = emails_df["message_content"] == rule_value
-                    else:
-                        if rule_key == "from":
-                            sender_email_query = ~emails_df["sender_email"] == rule_value
-                        elif rule_key == "subject":
-                            subject_query = ~emails_df["subject"] == rule_value
-                        else:
-                            message_query = ~emails_df["message_content"] == rule_value
+                    query_results.append(process_string_field_filter\
+                                             (emails_df, rule_key, rule_value, rule_predicate))
 
             # get the required results based on all/any
-            if global_predicate == "all":
-                email_results = emails_df[sender_email_query & subject_query & message_query & date_query]
-            else:
-                email_results = emails_df[sender_email_query | subject_query | message_query | date_query]
+            email_results = combine_all_filters(emails_df, query_results, global_predicate)
 
             # capture the message ids
             filtered_message_ids = email_results['message_id'].tolist()
@@ -102,25 +143,15 @@ def modify_emails(gmail_creds):
             # instantiate gmail service
             service = build('gmail', 'v1', credentials=gmail_creds)
 
-            # all possible labels for emails
-            all_label_ids = ["INBOX", "SPAM", "TRASH", "UNREAD"]
+            # capture the actions that needs to be performed with the filtered messages
+            move_message_label_id = rules[rule_name]["actions"]["move_message_to"].upper()
+            message_read_label_id = rules[rule_name]["actions"]["mark_message_as"].upper()
 
-            # list of label_ids to associated with the filtered messages
-            add_label_ids = []
-
-            # capture the action from the rule and add it to add_label_ids, remove it from all_label_ids
-            # the filtered messages will be associated with all_label_ids and will be disassociated from the
-            # rest of the label_ids represented by all_label_ids
-            move_label_id = rules[rule_name]["actions"]["move_message_to"].upper()
-            add_label_ids.append(move_label_id)
-            all_label_ids.remove(move_label_id)
-
-            read_label_id = rules[rule_name]["actions"]["mark_message_as"].upper()
-            # if the message needs to marked as unread, add it to add_label_ids and remove it from all_label_ids
-            if read_label_id == "UNREAD":
-                all_label_ids.remove(read_label_id)
-                add_label_ids.append(read_label_id)
+            # determine the labels to be added/removed to filtered messages based on the actions specified
+            add_label_ids, remove_label_ids = prepare_labels_for_filtering_emails\
+                (move_message_label_id, message_read_label_id)
             request_body = {'ids': filtered_message_ids, 'addLabelIds': add_label_ids, \
-                            'removeLabelIds': all_label_ids}
-            # call the google gmail batchModify API
+                            'removeLabelIds': remove_label_ids}
+
+            # call the google gmail batchModify API to add/remove determined labels on Gmail
             service.users().messages().batchModify(userId='me', body=request_body).execute()
